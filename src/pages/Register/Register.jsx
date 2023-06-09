e7862adc4b393d7a1482c0a526c81f8c7308dcf0
Mridul1x <mridulhasannn@gmail.com>
2023-06-09 06:21:26 +0600

    Navbar Created

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Coming soon</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/utilities/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYLsaSdaHl8V8yOleLd7Y8tYH-ccsF49U",
+  authDomain: "summercamp-d6cf0.firebaseapp.com",
+  projectId: "summercamp-d6cf0",
+  storageBucket: "summercamp-d6cf0.appspot.com",
+  messagingSenderId: "418955799499",
+  appId: "1:418955799499:web:edd557e8fa4430bddf644e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
